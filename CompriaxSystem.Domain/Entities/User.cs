aed0eedb99ecdcbd8f7d4169f6fb2bac82d2dc62
Joaquin Vargas <vargasjoaquin9978@gmail.com>
2026-09-01 17:39:42 -0300

    Se agrean las entidades de rol y usuario

---
+++ CompriaxSystem.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using CompriaxSystem.Domain.Common;
+
+namespace CompriaxSystem.Domain.Entities
+{
+    public class User : AuditableEntity
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Email { get; set; }
+        public bool IsActive { get; set; } = true;
+        public byte[]? Photo { get; set; }
+
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; } = null!;
+    }
+}
