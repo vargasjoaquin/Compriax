fc458d3f4cbb1d94b31d4c899dd77069a6a86c41
Joaquin Vargas <vargasjoaquin9978@gmail.com>
2026-09-02 01:24:59 -0300

    se agrega unos servicios de seguridad, huella digital de hardware e criptografia

---
+++ CompriaxSystem.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,10 @@
+﻿using CompriaxSystem.Application.Interfaces.Repositories;
+
+namespace CompriaxSystem.Infrastructure.Security
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+        public bool Verify(string password, string hash) => BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
