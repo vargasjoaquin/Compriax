a9db11aded8b76eaffc31920e4c3868b5ff803a4
Joaquin Vargas <vargasjoaquin9978@gmail.com>
2026-09-09 18:28:34 -0300

    Se crean las clases correspondientes a la configuracion  y envio y respuesta del pago mediante el qr de mercado pago.

---
+++ CompriaxSystem.MercadoPago.Api/Configuration/MercadoPagoSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace CompriaxSystem.MercadoPago.Api.Configuration
+{
+    public class MercadoPagoSettings
+    {
+        public bool Enabled { get; set; }
+        public string PublicKey { get; set; } = null!;
+        public string AccessToken { get; set; } = null!;
+        public string WebhookSecret { get; set; } = null!;
+    }
+}
