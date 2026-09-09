e4cfc10c2d277b5334fc2958540fb493c7073802
Joaquin Vargas <vargasjoaquin9978@gmail.com>
2026-09-09 19:39:26 -0300

    Se actualiza el archivo program realizando inyeccion de dependencias de los servicios, interfacez, configuraciones, etc

--- CompriaxSystem.MercadoPago.Api/Program.cs
+++ CompriaxSystem.MercadoPago.Api/Program.cs
@@ -1,23 +1,46 @@
+using CompriaxSystem.Infrastructure.Persistence;
+using CompriaxSystem.MercadoPago.Api.Configuration;
+using CompriaxSystem.MercadoPago.Api.Interfaces;
+using CompriaxSystem.MercadoPago.Api.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<MercadoPagoSettings>(builder.Configuration.GetSection("MercadoPago"));
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? "Server=.;Database=Compriax;Integrated Security=True;TrustServerCertificate=True";
+
+    options.UseSqlServer(connectionString);
+});
+
+
 // Add services to the container.
+builder.Services.AddScoped<IMercadoPagoService, MercadoPagoService>();
+builder.Services.AddScoped<ITransactionService, TransactionService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "CompriaxSystem Mercado Pago API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
