9fed73a60b5348c85d6e790f81f9d2061a2ba5d6
Joaquin Vargas <vargasjoaquin9978@gmail.com>
2026-09-09 18:26:46 -0300

    Se crea en la capa de dominio las clases de las transacciones de pago de mercado pago y sus estados

---
+++ CompriaxSystem.Domain/Enums/MercadoPagoPaymentStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace CompriaxSystem.Domain.Enums
+{
+    public enum MercadoPagoPaymentStatus
+    {
+        Pending = 1,
+        Approved = 2,
+        Rejected = 3,
+        Canceled = 4,
+        Expired = 5,
+    }
+}
