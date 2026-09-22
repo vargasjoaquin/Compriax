8ecca3a1cb8bc4dc580aa9a2587f889d9d18b9a8
Joaquin Vargas <vargasjoaquin9978@gmail.com>
2026-09-22 15:47:05 -0300

    refactor: se agregan nuevos validadores.

---
+++ CompriaxSystem.Application/Validations/CashMovementCreateValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using CompriaxSystem.Application.DTOs;
+
+namespace CompriaxSystem.Application.Validations
+{
+    public class CashMovementCreateValidator : AbstractValidator<CashMovementCreateDto>
+    {
+        public CashMovementCreateValidator()
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(x => x.Amount)
+                .GreaterThan(0).WithMessage("El monto del movimiento debe ser mayor a $ 0.00.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Debe ingresar una descripción para el movimiento.")
+                .MaximumLength(200).WithMessage("La descripción no puede superar los 200 caracteres.");
+
+            RuleFor(x => x.MovementType)
+                .IsInEnum().WithMessage("El tipo de movimiento de caja no es válido.");
+        }
+    }
+}
---
+++ CompriaxSystem.Application/Validations/CashShiftOpenValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using CompriaxSystem.Application.DTOs;
+
+namespace CompriaxSystem.Application.Validations
+{
+    public class CashShiftOpenValidator : AbstractValidator<CashShiftOpenDto>
+    {
+        public CashShiftOpenValidator()
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(x => x.InitialCash)
+                .GreaterThanOrEqualTo(0).WithMessage("El fondo inicial de caja no puede ser negativo.");
+        }
+    }
+}
